40e48005ced3117052d9d9fdcbb5ac39ad8106e4
N00byKing <N00byKing@hotmail.de>
2018-11-16 19:26:13 +0100

    ???

--- system_prop.mk
+++ system_prop.mk
@@ -157,3 +157,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     telephony.lteOnGsmDevice=1 \
     telephony.lteOnCdmaDevice=1
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+
